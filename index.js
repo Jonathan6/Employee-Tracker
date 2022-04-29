35722f181cee7019a2758ecd199ec0fff5a73539
Jonathan Chen <jontkc98@uw.edu>
2022-04-29 15:08:40 -0700

    The starting pseudo code for employee-tracker. Focus on javascript. Need SQL db and express routes

---
+++ index.js
@@ -0,0 +1,84 @@
+// FAKE CODE :)
+
+
+// For example they chooose view all departments
+function viewAllDepartments() {
+    // we run a sql query to get all the departments info
+    // this involves calling the right express route through JS
+    // express route fills out a proper query and makes the call to SQL
+    // gets the information then returns it back to here
+    // we then display the info using the table package
+}
+
+function viewAllRoles() {
+    // Make express call for all the roles
+    // express handles the rest
+    // we run a sql query to get all the available roles
+    // send the information back to JS
+    // we then display the info using the table package
+}
+
+function viewAllEmployees() {
+    // Make express call for all the current employees
+    // express handles the rest
+    // we run a sql query to get all the current employees
+    // send the information back to JS
+    // we then display the info using the table package
+
+}
+
+function addDepartment() {
+    // Make express call to add a department
+    // express handles the rest
+    // we run a sql mutation to add a department to the list
+    // send the information back to JS
+    // we confirm or deny the request
+
+}
+
+function addRole() {
+    // Make express call to add a role
+    // express handles the rest
+    // we run a sql mutation to add a role to the list
+    // send the information back to JS
+    // we confirm or deny the request
+
+}
+
+function addEmployee() {
+    // Make express call to add a employee
+    // express handles the rest
+    // we run a sql mutation to add a employee to the list
+    // send the information back to JS
+    // we confirm or deny the request
+
+}
+
+function updateEmployeeRole() {
+    // Make express call to update an employee
+    // express handles the rest
+    // we run a sql mutation to update an employee based on id
+    // send the information back to JS
+    // we confirm or deny the request
+
+}
+
+function quit() {
+    // Are you sure prompt
+    // Return opposite of their answer
+}
+
+function init() {
+    // Run cool into logo page print 
+    let gate = true;
+    while (gate) {
+        // Display them the different options
+        // Get response back
+        // Run a function depending on their answer
+    }
+    // See you next time :) screen
+}
+
+
+// User launches the program we start with init()
+init()
